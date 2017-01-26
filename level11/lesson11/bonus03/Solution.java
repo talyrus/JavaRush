bddf665d6371d746367f1f116adcadbfa0527bae
Taly <talyrus@gmail.com>
2017-01-26 09:10:29 +0300

    Решение задач курса JavaRush

---
+++ level11/lesson11/bonus03/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.test.level11.lesson11.bonus03;
+
+/* Задача по алгоритмам
+Написать метод, который возвращает минимальное и максимальное числа в массиве.
+*/
+
+import java.lang.reflect.Array;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndMaximum(data);
+
+        System.out.println("Minimum is " + result.x);
+        System.out.println("Maximum is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndMaximum(int[] array)
+    {
+        if (array == null || array.length == 0)
+        {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        //Напишите тут ваше решение
+        for (int i = array.length - 1; i > 0 ; i--) // реализация сортировки "пузырьком"
+        {
+            for (int j = 0; j < array.length - 1 ; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        return new Pair<Integer, Integer>(array[0], array[9]); // возвращаем первый (минимальный) и последний (максимальный) элементы массива
+    }
+
+
+    public static class Pair<X, Y>
+    {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
