9e559a893ed8ff9d194ffd85b6c094859a072cff
Taly <talyrus@gmail.com>
2017-01-27 10:50:03 +0300

    Решение задач курса JavaRush

---
+++ level12/lesson09/task02/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level12.lesson09.task02;
+
+/* Интерфейсы Fly, Run, Swim
+Напиши public интерфейсы Fly(летать), Run(бежать/ездить), Swim(плавать).
+Добавить в каждый интерфейс по одному методу.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    //add interfaces here - добавь интерфейсы тут
+    public interface Fly
+    {
+        void Method1();
+    }
+
+    public interface Run
+    {
+        void Method2();
+    }
+
+    public interface Swim
+    {
+        void Method3();
+
+    }
+}
